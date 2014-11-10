c49a8bcef8521ab09c401f15b25e2fefbe28870f
Taneli Hartikainen <taneli.hartikainen@jamk.fi>
2014-11-10 11:36:38 +0200

    Add KDA, Economy categories.

--- public/src/scripts/views/week.jsx
+++ public/src/scripts/views/week.jsx
@@ -58,20 +58,27 @@ module.exports = React.createClass({
 
 		return (
 			<div className="week-view">
+
+				{/* Week Picker */}
 				<div className="row">
 					<div className="col-xs-12 week-picker">
 						<WeekPicker year={year} week={week} />
 					</div>
 				</div>
 
+				{/* Rankings by category */}
 				<div className="row">
-					<div className="col-xs-12 col-sm-6">
-						<StatsList track="kills" title="Total Kills"
-						           matches={this.state.matches} />
+
+					{/* Kills, Deaths Assists go here... */}
+					<div className="col-xs-12">
+						<div className="panel panel-default">
+							<div className="panel-heading">KDA</div>
+						</div>
 					</div>
 
+					{/* Kills */}
 					<div className="col-xs-12 col-sm-6">
-						<StatsList track="deaths" title="Total Deaths"
+						<StatsList track="kills" title="Total Kills"
 						           matches={this.state.matches} />
 					</div>
 
@@ -81,13 +88,68 @@ module.exports = React.createClass({
 						           matches={this.state.matches} />
 					</div>
 
+					{/* Deaths */}
+					<div className="col-xs-12 col-sm-6">
+						<StatsList track="deaths" title="Total Deaths"
+						           matches={this.state.matches} />
+					</div>
+
 					<div className="col-xs-12 col-sm-6">
 						<StatsList track="deaths" average={true}
 						           title="Average Deaths"
 						           matches={this.state.matches} />
 					</div>
+
+					{/* Assists */}
+					<div className="col-xs-12 col-sm-6">
+						<StatsList track="assists" title="Total Assists"
+						           matches={this.state.matches} />
+					</div>
+
+					<div className="col-xs-12 col-sm-6">
+						<StatsList track="assists" average={true}
+						           title="Average Assists"
+						           matches={this.state.matches} />
+					</div>
+
+					{/* XPM, GPM, Last hits, Denies go here... */}
+					<div className="col-xs-12">
+						<div className="panel panel-default">
+							<div className="panel-heading">Economy</div>
+						</div>
+					</div>
+
+					{/* XPM */}
+					<div className="col-xs-12 col-sm-6">
+						<StatsList track="xpm" average={true}
+						           title="Average XP Per Minute"
+						           matches={this.state.matches} />
+					</div>
+
+					{/* GPM */}
+					<div className="col-xs-12 col-sm-6">
+						<StatsList track="gpm" average={true}
+						           title="Average Gold Per Minute"
+						           matches={this.state.matches} />
+					</div>
+
+					{/* Last Hits */}
+					<div className="col-xs-12 col-sm-6">
+						<StatsList track="lastHits" average={true}
+						           title="Average Last Hits"
+						           matches={this.state.matches} />
+					</div>
+
+					{/* Denies */}
+					<div className="col-xs-12 col-sm-6">
+						<StatsList track="denies" average={true}
+						           title="Average Denies"
+						           matches={this.state.matches} />
+					</div>
+
 				</div>
 
+				{/* Match History */}
 				<div className="row">
 					<div className="col-xs-12 match-list">
 						<MatchList matches={this.state.matches} />
